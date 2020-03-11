44d546f9da1347fe45a3a13c0bc446567506a568
Mike C <Meekaiell@github.com>
2020-03-11 17:58:57 -0400

    idk what this is

---
+++ cmake-build-debug/CMakeFiles/canny.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/canny.dir/canny.c.o"
+  "CMakeFiles/canny.dir/dc_image.c.o"
+  "canny.exe"
+  "canny.pdb"
+  "libcanny.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/canny.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
